ece2f9798e57f004a40f55ac7404c9cd38067e5f
KwabenaEpic <jshafari@gmail.com>
2023-02-03 05:33:44 +0000

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,10 @@
-// module.exports = {
-//   content: [
-//     "./pages/**/*.{js,ts,jsx,tsx}",
-//     "./components/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
 module.exports = {
-  // content: [], <-- not working
-  // content: ['./pages/**/*.{js,ts,jsx,tsx}'], <-- not working
-  content: ['apps/site/pages/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-};
+  plugins: [],
+}
