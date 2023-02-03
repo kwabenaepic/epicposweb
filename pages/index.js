b869812962967e4c9fae9381cb09183bba20d208
KwabenaEpic <jshafari@gmail.com>
2023-02-03 10:34:12 +0000

    .

--- pages/index.js
+++ pages/index.js
@@ -3,31 +3,31 @@ import HomeDashboard from "../components/HomeDashboard";
 import Login from "./login";
 
 const Navigatior = () => {
-  const isLoggedin = false;
-  // if (!isLoggedin) {
-  //   return (
-  //     <div className="flex flex-row h-screen w-screen">
-  //       <Login />
-  //     </div>
-  //   );
-  // }
-  // return (
-  //   <div className="flex h-screen w-full">
-  //     <div className="flex flex-col ">
-  //       <div className="bg-red-300 h-10 w-full"></div>
-  //       <div className="flex flex-row h-screen w-full">
-  //         <LeftNavigation />
-  //         <HomeDashboard />
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
+  const isLoggedin = true;
+  if (!isLoggedin) {
+    return (
+      <div className="flex flex-row h-screen w-screen">
+        <Login />
+      </div>
+    );
+  }
+  return (
+    <div className="flex h-screen w-full">
+      <div className="flex flex-col ">
+        <div className="bg-red-300 h-10 w-full"></div>
+        <div className="flex flex-row h-screen w-full">
+          <LeftNavigation />
+          <HomeDashboard />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default function Home() {
   return (
     <div className="flex h-screen w-full">
-     <Login />
+     <Navigatior />
     </div>
   );
 }
